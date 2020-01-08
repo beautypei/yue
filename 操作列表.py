5c7d40c4bdc878ef3cf2b2edc82ad51b7ae6e97c
beautypei <pei_yue@163.com>
2020-01-08 21:24:04 +0800

    练习

--- 操作列表.py
+++ 操作列表.py
@@ -10,3 +10,40 @@ magicians = ['alice', 'david', 'carolina']
 for magician in magicians:#冒号里面的部分都是for循环执行的部分
     print(magician.title() + ', that was a great trick!')
     print("I can't wait to see your next trick, " + magician.title() + '.\n')
+
+for value in range(1,6):
+    print(value)#使用range函数打印数字，但是打印从1开始到5结束，不包含6
+
+numbers = list(range(1,6))
+print(numbers)#用list打印一个数字列表
+
+even_numbers = list(range(2,11,2))
+print(even_numbers)
+
+squares = []
+for value in range(1,11):
+    square = value ** 2#两个**表示乘方
+    squares.append(square)#将乘方加到列表的后面
+print(squares)
+
+squares = []
+for value in range(1,11):
+    squares.append(value ** 2)#直接将value的乘方加到列表的后面
+print(squares)
+
+digits = [1,2,3,4,5,6,7,8,9,0]
+print(min(digits)) #打印列表的最小值
+print(max(digits)) #打印列表的最大值
+print(sum(digits)) #打印列表值的和
+
+squares = [value ** 2 for value in range(1,11)]
+print(squares) #列表解析
+
+squares = [value ** 3 for value in range(1,11)]
+print(squares) #练习，打印1-10的立方
+
+
+squares = []
+for value in range(1,21):
+    squares.append(value)
+print(squares) #练习，打印列表的1-20
