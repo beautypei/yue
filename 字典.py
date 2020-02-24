6ef25ae8679aa328a9730d086d33178d89bc534a
beautypei <pei_yue@163.com>
2020-02-25 00:58:58 +0800

    测试

--- 字典.py
+++ 字典.py
@@ -1,3 +1,40 @@
-alien_0 = {'color' : 'green','points':5}
+alien_0 = {'color': 'green','points': 5}
 print(alien_0['color'])
-print(alien_0['points'])
+print(alien_0['points']) #创建一个字典
+
+alien_0 = {'color': 'green','points': 5}
+new_points = alien_0['points'] #设置一个变量
+print('You just earned ' + str(new_points) + ' points!')
+
+alien_0 = {'color': 'green','points': 5}
+print(alien_0)
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25 #在字典中添加键-值对
+print(alien_0)
+
+alien_0 = {} #创建一个空字典并添加键-值对
+alien_0['color'] = 'green'
+alien_0['points'] = 5
+print(alien_0)
+
+alien_0 = {'color':'green'}
+print('The alien is ' + alien_0['color'] + '.')
+alien_0['color'] = 'yellow'#修改字典中的值
+print('The alien is now ' + alien_0['color'] + '.')
+
+alien_0 = {'x_position':0,'y_position':25,'speed':'medium'}
+print('Original x_position: ' + str(alien_0['x_position']))
+alien_0['speed'] = 'fast'
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print('New x_position: ' + str(alien_0['x_position']))
+
+alien_0 = {'color':'green','points':5}
+print(alien_0)
+del alien_0['points'] #删除键值对，永久删除
+print(alien_0)
